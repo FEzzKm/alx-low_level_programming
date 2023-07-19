6f387617f57cedcbb1e655c4d0b49bacfdc7c19b
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-07-19 12:45:13 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - utilizes on the _putchar function to print
- *                 the alphabet a - z 10 times
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
 */
 
 void print_alphabet_x10(void)
 {
-	int c;
-	int j;
+	int c, j;
+
 	for (j = 0; j < 10; ++j)
 	{
 		for (c = 'a'; c <= 'z'; ++c)
