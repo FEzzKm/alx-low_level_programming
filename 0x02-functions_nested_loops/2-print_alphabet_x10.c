54c30a87dc6d7815f307750a7db9bb433b7b2c37
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-07-19 12:29:57 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,9 @@ void print_alphabet_x10(void)
 {
         int c;
 	int j; 
-	while (j<10)
-	{
+	for (j = 0; j < 10; j++)
+	
 		for (c = 'a'; c <= 'z'; ++c)
 			_putchar(c);
 		_putchar('\n');
-	}
 }
