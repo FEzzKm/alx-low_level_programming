0897a716a97b6763bb3ed9d4c7e6b7561a191e9e
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-07-18 19:47:00 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: prints alphabets
+ *
+ * Return: Always 0.
+ *
+ **/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		_putchar(c);
+	_putchar('\n');
+}
