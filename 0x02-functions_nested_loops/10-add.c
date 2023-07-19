9e2f2439e21686ef3aa728c28e197004ad6dbbe4
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-07-19 14:55:29 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @n: first input
+ * @m: second input
+ *
+ * Return: sum of n & m
+*/
+
+int add(int n, int m)
+{
+	int s = n + m;
+
+	return (s);
+}
