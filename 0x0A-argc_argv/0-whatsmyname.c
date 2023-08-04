8049565d8a3a5933f40e8a82891dcef21898c992
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-08-04 04:44:14 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - function to give program name
+ * @argc : number of args
+ * @argv : array of strings
+ * Return : 0
+ *
+*/  
+
+int main (int argc, char *argv[])
+{
+  int count;
+
+  printf ("This program was called with \"%s\".\n",argv[0]);
+
+  if (argc > 1)
+    {
+      for (count = 1; count < argc; count++)
+	{
+	  printf("argv[%d] = %s\n", count, argv[count]);
+	}
+    }
+  else
+    {
+      printf("The command had no other arguments.\n");
+    }
+
+  return 0;
+}
