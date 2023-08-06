5eae9bc2972449bf6dcd1f81cc0a028801bf5d43
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-08-06 05:58:05 -0700

    1

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function to check if
+ *	c is lowercase
+ *
+ * @c: check input of function
+ *	if lowercase
+ *
+ * Return: returns 1 if so else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
