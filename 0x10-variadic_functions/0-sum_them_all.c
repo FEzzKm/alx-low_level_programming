07ff1893dd66abba7251a5a65d272b0ddf41394d
FEzzKm <elkacem.fatima.ezzahra@gmail.com>
2023-08-17 12:11:44 -0700

    h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int param, sum = 0;
+
 	va_start(ap, n);
+
 	for (param = 0; param < n; param++)
 		sum += va_arg(ap, int);
 	va_end(ap);
